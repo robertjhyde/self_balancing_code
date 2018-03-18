e5ba4d9d7c84ca0c15053ea37c46bec66577cf6d
Nirav <32882197+NiravGC@users.noreply.github.com>
2018-03-18 17:41:21 +0000

    Update moves.py

--- moves.py
+++ moves.py
@@ -2,7 +2,30 @@ from motor import MOTOR
 
 motor = MOTOR()
 
-def F():  
+def F():    # forward
+  print('Move forward')
   pass
   
-def FS():
+def FS():   # forward slow
+  print('Move forward slowly')
+  pass
+
+def B():    # backwards
+  print('Move backwards')
+  pass
+
+def R():    # right
+  print('Turn right')
+  pass
+
+def RS():   # right slow
+  print('Turn right slowly')
+  pass
+
+def L():    # left
+  print('Turn left')
+  pass
+
+def LS():   # left slow
+  print('Turn left slowly')
+  pass
