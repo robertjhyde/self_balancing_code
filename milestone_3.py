c1130eac51db94936bb2ce164537b497ea82ba59
Nirav <32882197+NiravGC@users.noreply.github.com>
2018-03-19 14:19:34 +0000

    updated to use motor class

--- milestone_3.py
+++ milestone_3.py
@@ -2,34 +2,25 @@ import pyb
 from pyb import Pin, Timer, ADC, DAC, LED
 from array import array				# need this for memory allocation to buffers
 from oled_938 import OLED_938			# Use OLED display driver
+from motor import MOTOR
 import micropython
+import time
 micropython.alloc_emergency_exception_buf(100)	# memory buffer for interrupts
 
-print('MILESTONE 2')
+print('MILESTONE 3')
 
 # ----- OLED CONFIG ----- #
 oled = OLED_938(pinout={'sda': 'Y10', 'scl': 'Y9', 'res': 'Y8'}, height=64, external_vcc=False, i2c_devid=61)
 oled.poweron()
 oled.init_display()
 
-# ----- WHEEL ASSIGNMENT ----- #
-A1 = Pin('X3', Pin.OUT_PP)		    # Control direction of motor A
-A2 = Pin('X4', Pin.OUT_PP)
-PWMA = Pin('X1')				    # Control speed of motor A
-B2 = Pin('X7', Pin.OUT_PP)		    # Control direction of motor B
-B1 = Pin('X8', Pin.OUT_PP)
-PWMB = Pin('X2')				    # Control speed of motor B
-
-# ----- TIMER ASSIGNMENT ----- #
-tim = Timer(2, freq = 1000)
-motorA = tim.channel (1, Timer.PWM, pin = PWMA)
-motorB = tim.channel (2, Timer.PWM, pin = PWMB)
-
 # -- MICROPHONE CONFIG -- #
 mic = ADC(Pin('Y11'))
 MIC_OFFSET = 1523		# ADC reading of microphone for silence
 dac = pyb.DAC(1, bits=12)  	# Output voltage on X5 (BNC) for debugging
 b_LED = LED(4)			# flash for beats on blue LED
+
+motor = MOTOR()			# use provided motor class
 
 N = 160					# size of sample buffer s_buf[]
 s_buf = array('H', 0 for i in range(N)) # reserve buffer memory
@@ -40,85 +31,62 @@ def flash():				# routine to flash blue LED when beat detected
 	b_LED.on()
 	pyb.delay(20)
 	b_LED.off()
-
-def energy(buf):			# Compute energy of signal in buffer
+	
+def energy(buf):			# Compute energy of signal in buffer 
 	sum = 0
 	for i in range(len(buf)):
 		s = buf[i] - MIC_OFFSET	# adjust sample to remove dc offset
 		sum = sum + s*s		# accumulate sum of energy
 	return sum
 
-# ------------------------- functions section ------------------------- #
+# ------------------------- dance moves ------------------------- #
 
-def forward(speed):
-	print('Move forward')
-	A1.high()
-	A2.low()
-	B1.high()
-	B2.low()
-	motorA.pulse_width_percent(speed)
-	motorB.pulse_width_percent(speed)
+def forward(speed):			# forwards
+	print('forward')
+	motor.A_forward(speed)
+	motor.B_forward(speed)
 	
-def forwardslow(speed):
-	print('Move forward slowly')
-	A1.high()
-	A2.low()
-	B1.high()
-	B2.low()
-	motorA.pulse_width_percent(speed/2)
-	motorB.pulse_width_percent(speed/2)
+def forwardslow(speed):			# slow forwards
+	print('forward slowly')
+	motor.A_forward(speed/2)
+	motor.B_forward(speed/2)
+
+def back(speed):			# backwards
+	print('back')
+	motor.A_back(speed/2)
+	motor.B_back(speed/2)
 	
-def back(speed):
-	print('Move back')
-	A1.low()
-	A2.high()
-	B1.low()
-	B2.high()
-	motorA.pulse_width_percent(speed/2)
-	motorB.pulse_width_percent(speed/2)
+def left(speed):			# left
+	print('left')
+	motor.A_back(speed/2)
+	motor.B_forward(speed)
+		
+def leftslow(speed):			# slow left
+	print('left slowly')
+	motor.A_back(speed/4)
+	motor.B_forward(speed/2)
 	
-def right(speed):
-	print('Move right')
-	A1.high()
-	A2.low()
-	B1.low()
-	B2.high()
-	motorA.pulse_width_percent(speed)
-	motorB.pulse_width_percent(speed/2)
+def right(speed):			# right
+	print('right')
+	motor.A_forward(speed)
+	motor.B_back(speed/2)
 	
-def rightslow(speed):
-	print('Move right slowly')
-	A1.high()
-	A2.low()
-	B1.low()
-	B2.high()
-	motorA.pulse_width_percent(speed/2)
-	motorB.pulse_width_percent(speed/4)
+def rightslow(speed):			# slow right
+	print('right slowly')
+	motor.A_forward(speed/2)
+	motor.B_back(speed/4)
 	
-def left(speed):
-	print('Move left')
-	A1.low()
-	A2.high()
-	B1.high()
-	B2.low()
-	motorA.pulse_width_percent(speed/2)
-	motorB.pulse_width_percent(speed)
-	
-def leftslow(speed):
-	print('Move left slowly')
-	A1.low()
-	A2.high()
-	B1.high()
-	B2.low()
-	motorA.pulse_width_percent(speed/4)
-	motorB.pulse_width_percent(speed/2)
+def stop(speed):			# stop
+	print('right slowly')
+	motor.A_stop()
+	motor.B_stop()
 
 # ------------------------- interrupt section ------------------------- #
 
 # ----- ISR FOR SAMPLING DATA ----- #
 def isr_sampling(null): 		# timer interrupt at 8kHz
 	global ptr, buffer_full		# global variables
-
+	
 	s_buf[ptr] = mic.read()		# take a sample every timer interrupt
 	ptr += 1			# increment buffer pointer (index)
 	if (ptr == N):			# wraparound ptr - goes 0 to N-1
@@ -133,61 +101,64 @@ pyb.enable_irq()			# enable interrupt again
 
 # ------------------------- end of interrupt section ------------------------- #
 
-# ----- MAIN LOOP CONSTANTS ----- #
-M = 50					# number of instantaneous energy epochs to sum
-BEAT_THRESHOLD = 2.0			# threshold for c to indicate a beat
-SILENCE_THRESHOLD = 1.3			# threshold for c to indicate silence
-MOVELIST = []				# empty list of moves to add to
-MOVE = 'w'				# current move: to be updated
-MOVEFUNC = {				# corresponding dance function for each move in list
-	'F': forward
-	'FS': forwardslow
-	'B': back
-	'R': right
+# ---- MAIN LOOP CONSTANTS ---- #
+M = 50				# number of instantaneous energy epochs to sum
+BEAT_THRESHOLD = 2.0		# threshold for c to indicate a beat
+SILENCE_THRESHOLD = 1.3		# threshold for c to indicate silence
+POS = 0                     	# position in movelist
+SPEED = 50                  	# baseline motor speed
+MOVELIST = []               	# empty list of moves to add to
+MOVE = 'w'                  	# current move: to be updated
+MOVEFUNC = {
+	'F': forward,
+	'FS': forwardslow,
+	'B': back,
+	'L': left,
+	'LS': leftslow,
+	'R': right,
 	'RS': rightslow
-	'L': left
-	'LS': leftslow
 }
 
-# ----- IMPORT MOVES ----- #
-with open('movelist.txt') as file:	# read movelist.txt
-	for line in file:		# take each line (i.e. each move)
-		line = line.strip()	# remove additional formatting (i.e. \n)
-		MOVELIST.append(line)	# add move to movelist
-print(MOVELIST)				# debug
+# ------- IMPORT MOVES -------- #
+with open('movelist.txt') as file:  	# read movelist.txt
+    for line in file:  			# take each line (i.e. each move)
+        line = line.strip()  		# remove additional formatting (i.e. \n)
+        MOVELIST.append(line) 		# add move to movelist
+print(MOVELIST)  			# debug
 
-# initialise variables for main program loop
+# initialise variables for main program loop 
 e_ptr = 0					# pointer to energy buffer
 e_buf = array('L', 0 for i in range(M)) 	# reserve storage for energy buffer
 sum_energy = 0					# total energy in last 50 epochs
 oled.draw_text(5,20, 'MILESTONE 2: Ready')	# Useful to show what's happening?
 oled.display()
 pyb.delay(100)
-tic = pyb.millis()		# mark time now in msec
+tic = pyb.millis()				# mark time now in msec
 
 # ----- MAIN PROGRAM LOOP ----- #
 while True:
 	if buffer_full:		# semaphore signal from ISR - set if buffer is full
-
+		
 		# Calculate instantaneous energy
 		E = energy(s_buf)
-
+		
 		# compute moving sum of last 50 energy epochs
 		sum_energy = sum_energy - e_buf[e_ptr] + E
 		e_buf[e_ptr] = E		# over-write earlest energy with most recent
 		e_ptr = (e_ptr + 1) % M		# increment e_ptr with wraparound - 0 to M-1
-
+		
 		# Compute ratio of instantaneous energy/average energy
 		c = E*M/sum_energy
-
+		dac.write(min(int(c*4095/3), 4095)) 	# useful to see on scope, can remove
+		
 		if (pyb.millis()-tic > 400):		# if more than 400ms since last beat -
 			if (c>BEAT_THRESHOLD):		# look for a beat
-				flash()			# beat found, flash blue LED
-				MOVE = MOVELIST[POS]	# take current move from list
-				if MOVE != 'w':			# 'w' = no move (wait)
-					STEP = MOVEFUNC[MOVE]	# find correct function for movement
-					STEP()			# execute dance move
-					print(STEP)		# debug
-				POS += 1		# ready for next move
-				tic = pyb.millis()	# reset tic
-		buffer_full = False			# reset status flag
+				flash()				# beat found, flash blue LED
+				MOVE = MOVELIST[POS]		# find next move in movelist
+				if MOVE != 'w':			# if not the 'wait' move
+					STEP = MOVEFUNC[MOVE]	# find corresponding dance function
+					STEP(SPEED)		# execute dance function
+				POS += 1			# move to next step
+				print(MOVE)			# debug
+				tic = pyb.millis()		# reset tic
+		buffer_full = False				# reset status flag
