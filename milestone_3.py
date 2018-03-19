43c1dd087de07894c4e2860b2e8e8302a7bbe17d
Nirav <32882197+NiravGC@users.noreply.github.com>
2018-03-19 10:29:37 +0000

    Create milestone_3.py

---
+++ milestone_3.py
@@ -0,0 +1,6 @@
+########## MILESTONE 3 ##########
+#   Process music in real time  #
+#   Sync dance moves to music   #
+#           Authors:            #
+# Nirav Ganju-Cass, Robert Hyde #
+# ----------------------------- #
