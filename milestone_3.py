ac8e4eae806e788599caa0c7694252e98c730923
Nirav <32882197+NiravGC@users.noreply.github.com>
2018-03-19 17:29:38 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,164 +1,124 @@
+'''    Start of comment section
+-------------------------------------------------------
+Name: Basic Beat Detection implementation using microphone class
+Creator:  Peter YK Cheung
+Date:   16 March 2017
+Revision:  1.5
+-------------------------------------------------------
+'''
 import pyb
+import time
+import micropython
 from pyb import Pin, Timer, ADC, DAC, LED
 from array import array				# need this for memory allocation to buffers
 from oled_938 import OLED_938			# Use OLED display driver
-from motor import MOTOR
-import micropython
-import time
-micropython.alloc_emergency_exception_buf(100)	# memory buffer for interrupts
+from mic import MICROPHONE
+from moves import *
 
-print('MILESTONE 3')
 
-# ----- OLED CONFIG ----- #
-oled = OLED_938(pinout={'sda': 'Y10', 'scl': 'Y9', 'res': 'Y8'}, height=64, external_vcc=False, i2c_devid=61)
+#  The following two lines are needed by micropython 
+#   ... must include if you use interrupt in your program
+micropython.alloc_emergency_exception_buf(100)
+
+# I2C connected to Y9, Y10 (I2C bus 2) and Y11 is reset low active
+oled = OLED_938(pinout={'sda': 'Y10', 'scl': 'Y9', 'res': 'Y8'}, height=64,
+                   external_vcc=False, i2c_devid=61)  
 oled.poweron()
 oled.init_display()
+oled.draw_text(0,20, 'Milestone 3: Loading')
+oled.display()
 
-# -- MICROPHONE CONFIG -- #
-mic = ADC(Pin('Y11'))
-MIC_OFFSET = 1523		# ADC reading of microphone for silence
-dac = pyb.DAC(1, bits=12)  	# Output voltage on X5 (BNC) for debugging
-b_LED = LED(4)			# flash for beats on blue LED
 
-motor = MOTOR()			# use provided motor class
+# Create microphone object
+N = 160
+mic = MICROPHONE(Timer(7,freq=8000),ADC('Y11'),N)
 
-N = 160					# size of sample buffer s_buf[]
-s_buf = array('H', 0 for i in range(N)) # reserve buffer memory
-ptr = 0					# sample buffer index pointer
-buffer_full = False			# semaphore - ISR communicate with main program
+# define ports for microphone, LEDs and trigger out (X5)
+b_LED = LED(4)		# flash for beats on blue LED
 
-def flash():				# routine to flash blue LED when beat detected
-	b_LED.on()
-	pyb.delay(20)
-	b_LED.off()
-	
-def energy(buf):			# Compute energy of signal in buffer 
-	sum = 0
-	for i in range(len(buf)):
-		s = buf[i] - MIC_OFFSET	# adjust sample to remove dc offset
-		sum = sum + s*s		# accumulate sum of energy
-	return sum
+def flash():  # routine to flash blue LED when beat detected
+    b_LED.on()
+    pyb.delay(20)
+    b_LED.off()
 
-# ------------------------- dance moves ------------------------- #
-
-def forward(speed):			# forwards
-	print('forward')
-	motor.A_forward(speed)
-	motor.B_forward(speed)
-	
-def forwardslow(speed):			# slow forwards
-	print('forward slowly')
-	motor.A_forward(speed/2)
-	motor.B_forward(speed/2)
-
-def back(speed):			# backwards
-	print('back')
-	motor.A_back(speed/2)
-	motor.B_back(speed/2)
-	
-def left(speed):			# left
-	print('left')
-	motor.A_back(speed/2)
-	motor.B_forward(speed)
-		
-def leftslow(speed):			# slow left
-	print('left slowly')
-	motor.A_back(speed/4)
-	motor.B_forward(speed/2)
-	
-def right(speed):			# right
-	print('right')
-	motor.A_forward(speed)
-	motor.B_back(speed/2)
-	
-def rightslow(speed):			# slow right
-	print('right slowly')
-	motor.A_forward(speed/2)
-	motor.B_back(speed/4)
-	
-def stop(speed):			# stop
-	print('right slowly')
-	motor.A_stop()
-	motor.B_stop()
-
-# ------------------------- interrupt section ------------------------- #
-
-# ----- ISR FOR SAMPLING DATA ----- #
-def isr_sampling(null): 		# timer interrupt at 8kHz
-	global ptr, buffer_full		# global variables
-	
-	s_buf[ptr] = mic.read()		# take a sample every timer interrupt
-	ptr += 1			# increment buffer pointer (index)
-	if (ptr == N):			# wraparound ptr - goes 0 to N-1
-		ptr = 0
-		buffer_full = True	# set the flag (semaphore) for buffer full
-
-# ----- 125 MICROSEC INTERRUPT ----- #
-pyb.disable_irq()			# disable interrupt while configuring timer
-sample_timer = pyb.Timer(7, freq=8000)	# set timer 7 for 8kHz
-sample_timer.callback(isr_sampling)	# specify interrupt service routine
-pyb.enable_irq()			# enable interrupt again
-
-# ------------------------- end of interrupt section ------------------------- #
-
-# ---- MAIN LOOP CONSTANTS ---- #
-M = 50				# number of instantaneous energy epochs to sum
-BEAT_THRESHOLD = 2.0		# threshold for c to indicate a beat
-SILENCE_THRESHOLD = 1.3		# threshold for c to indicate silence
-POS = 0                     	# position in movelist
-SPEED = 50                  	# baseline motor speed
-MOVELIST = []               	# empty list of moves to add to
-MOVE = 'w'                  	# current move: to be updated
-MOVEFUNC = {
-	'F': forward,
-	'FS': forwardslow,
-	'B': back,
-	'L': left,
-	'LS': leftslow,
-	'R': right,
-	'RS': rightslow
+# Define constants for main program loop - shown in UPPERCASE
+M = 50						# number of instantaneous energy epochs to sum
+BEAT_THRESHOLD = 1.8		# threshold for c to indicate a beat
+SILENCE_THRESHOLD = 1.3  	# threshold for c to indicate silence
+position = 0  			# position in movelist
+s = 20  			# baseline motor speed
+move_list = []  		# empty list of moves to add to
+move_steps = {
+    'F': forward,
+    'FS': forwardslow,
+    'B': back,
+    'L': left,
+    'LS': leftslow,
+    'R': right,
+    'RS': rightslow,
+    'S': stop
 }
 
 # ------- IMPORT MOVES -------- #
-with open('movelist.txt') as file:  	# read movelist.txt
-    for line in file:  			# take each line (i.e. each move)
-        line = line.strip()  		# remove additional formatting (i.e. \n)
-        MOVELIST.append(line) 		# add move to movelist
-print(MOVELIST)  			# debug
+with open('movelist.txt') as file:  # read movelist.txt
+    for line in file:  # take each line (i.e. each move)
+        line = line.strip()  # remove additional formatting (i.e. \n)
+        move_list.append(line)  # add move to movelist
 
 # initialise variables for main program loop 
 e_ptr = 0					# pointer to energy buffer
-e_buf = array('L', 0 for i in range(M)) 	# reserve storage for energy buffer
+e_buf = array('L', 0 for i in range(M))		# reserve storage for energy buffer
 sum_energy = 0					# total energy in last 50 epochs
-oled.draw_text(5,20, 'MILESTONE 2: Ready')	# Useful to show what's happening?
-oled.display()
 pyb.delay(100)
 tic = pyb.millis()				# mark time now in msec
 
-# ----- MAIN PROGRAM LOOP ----- #
-while True:
-	if buffer_full:		# semaphore signal from ISR - set if buffer is full
+# ------- IDLE STATE -------- #
+print('Ready to begin Milestone 3')
+print('Waiting for button press')
+oled.clear()
+oled.draw_text(5, 20, 'MILESTONE 3: Ready')
+oled.draw_text(5, 40, 'Press USR button')
+oled.display()
+trigger = pyb.Switch()
+while not trigger():
+    time.sleep(0.001)
+while trigger(): pass
+print('Button pressed - running')
+oled.clear()
+oled.draw_text(5, 20, 'MILESTONE 3: Ready')
+oled.draw_text(20, 40, 'Running')
+oled.display()
+# real-time program loop
+
+try:
+	while True:				# Main program loop
+        	if mic.buffer_full():		# semaphore signal from ISR - set if buffer is full
+		# Get instantaneous energy
+			E = mic.inst_energy()	# get the instantaneous energy from the microphone
+
+            	# compute moving sum of last 50 energy epochs
+			sum_energy = sum_energy - e_buf[e_ptr] + E
+            		e_buf[e_ptr] = E		# over-write earliest energy with most recent
+            		e_ptr = (e_ptr + 1) % M	# increment e_ptr with wraparound - 0 to M-1
+
+			# Compute ratio of instantaneous energy/average energy
+			c = E*M/sum_energy
+			# Look for a beat
+			if (pyb.millis()-tic > 400):	# if more than 400ms since last beat
+				if (c>BEAT_THRESHOLD):
+					# look for a beat, or if not found, timeout
+					tic = pyb.millis()		# reset tic
+					flash()         # beat found, flash blue LED ON
+                    			next_step = move_list[position]
+                    			print(next_step)
+                    			if next_step != 'w':
+                        			step = move_steps[next_step]
+                        			step(s)
+                        			position += 1			# increment a counter when beat detected
+                        			tic = pyb.millis()
+			mic.set_buffer_empty()			# reset the buffer_full flag
+
+finally: # in the event of a crash or keyboard interrupt turn of motors before exiting program
+	motor.A_stop()
+	motor.B_stop()
 		
-		# Calculate instantaneous energy
-		E = energy(s_buf)
-		
-		# compute moving sum of last 50 energy epochs
-		sum_energy = sum_energy - e_buf[e_ptr] + E
-		e_buf[e_ptr] = E		# over-write earlest energy with most recent
-		e_ptr = (e_ptr + 1) % M		# increment e_ptr with wraparound - 0 to M-1
-		
-		# Compute ratio of instantaneous energy/average energy
-		c = E*M/sum_energy
-		dac.write(min(int(c*4095/3), 4095)) 	# useful to see on scope, can remove
-		
-		if (pyb.millis()-tic > 400):		# if more than 400ms since last beat -
-			if (c>BEAT_THRESHOLD):		# look for a beat
-				flash()				# beat found, flash blue LED
-				MOVE = MOVELIST[POS]		# find next move in movelist
-				if MOVE != 'w':			# if not the 'wait' move
-					STEP = MOVEFUNC[MOVE]	# find corresponding dance function
-					STEP(SPEED)		# execute dance function
-				POS += 1			# move to next step
-				print(MOVE)			# debug
-				tic = pyb.millis()		# reset tic
-		buffer_full = False				# reset status flag
